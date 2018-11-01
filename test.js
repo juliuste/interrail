3ff786a974fdbdc4d10918af0a849771ed0a8743
Julius Tens <mail@juliustens.eu>
2018-11-01 22:55:43 +0100

    soothe the linter

--- test.js
+++ test.js
@@ -6,29 +6,28 @@ const interrail = require('./index')
 tape('interrail.stations', (t) => {
 	interrail.stations('Berlin').then((results) => {
 		t.plan(9)
-		t.false(results.length==0, 'results count')
+		t.false(results.length === 0, 'results count')
 		t.true(results[0].type === 'station', 'result type')
 		t.true(results[0].id > 0, 'result id')
 		t.true(results[0].name, 'result name')
 		t.true(results[0].weight >= 0, 'result weight')
 		t.true(results[0].products, 'result products')
-		if(results[0].coordinates){
+		if (results[0].coordinates) {
 			t.true(results[0].coordinates.longitude, 'result coordinates longitude')
 			t.true(results[0].coordinates.latitude, 'result coordinates latitude')
 		}
-		t.true(results[0].name == 'BERLIN (Germany)', 'result for `Berlin`')
+		t.true(results[0].name === 'BERLIN (Germany)', 'result for `Berlin`')
 	}).catch(console.error)
 })
-
 
 tape('interrail.journeys', (t) => {
 	interrail.journeys(
 		8065969, // Berlin
 		7942300, // Ljubljana
-		(new Date).toISOString()
+		(new Date()).toISOString()
 	).then((results) => {
 		t.plan(14)
-		t.false(results.length==0, 'results count')
+		t.false(results.length === 0, 'results count')
 		t.true(results[0].type === 'journey', 'result type')
 		t.true(results[0].legs, 'result legs')
 		t.true(results[0].legs[0].origin.type === 'station', 'leg origin.type')
@@ -41,6 +40,6 @@ tape('interrail.journeys', (t) => {
 		t.true(results[0].legs[0].arrival, 'leg arrival')
 		t.true(+new Date(results[0].legs[0].departure) < +new Date(results[0].legs[0].arrival), 'leg departure before arrival')
 		t.true(results[0].legs[0].line, 'leg line')
-		t.true(results[0].legs[results[0].legs.length - 1].destination.name == 'LJUBLJANA (Slovenia)', 'result for trip to `Ljubljana`')
+		t.true(results[0].legs[results[0].legs.length - 1].destination.name === 'LJUBLJANA (Slovenia)', 'result for trip to `Ljubljana`')
 	}).catch(console.error)
 })
